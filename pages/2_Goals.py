2231324a222844f60ac2dc23578c80796a9a3bdc
bodapatlabhargavireddy <bodapatlabhagravireddy@gmail.com>
2026-03-16 21:49:14 +0530

    Update 2_Goals.py

--- pages/2_Goals.py
+++ pages/2_Goals.py
@@ -1,14 +1,15 @@
 import streamlit as st
 
 st.set_page_config(initial_sidebar_state="collapsed")
-st.markdown("<style>[data-testid='collapsedControl'] {display: none;}</style>", unsafe_allow_html=True)
 
 st.title("🎯 Step 4: Your Goal")
-st.selectbox("What is your target?", 
-             ["Maintenance", "Weight Loss", "Weight Gain"], 
-             key="goal")
+
+# Capture the goal in session_state
+st.selectbox("What is your primary target?", 
+             ["Maintenance", "Weight Loss", "Weight Gain", "Manage PCOD/PCOS", "Diabetes Control","Thyroid Control",BP Control], 
+             key="goal")
 
 st.progress(80)
 
 if st.button("🚀 Generate My Cura Dashboard"):
-    st.switch_page("pages/4_Dashboard.py")
+    st.switch_page("pages/4_Dashboard.py")
